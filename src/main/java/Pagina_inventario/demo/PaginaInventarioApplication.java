858edb40d3bbac64f60667dde23cea6eaeca0fc4
JesusB05 <113481424+JesusB05@users.noreply.github.com>
2022-10-03 19:41:10 -0600

    Tarea 1, Desarrollo Web

---
+++ src/main/java/Pagina_inventario/demo/PaginaInventarioApplication.java
@@ -0,0 +1,13 @@
+package Pagina_inventario.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaginaInventarioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PaginaInventarioApplication.class, args);
+	}
+
+}
---
+++ src/test/java/Pagina_inventario/demo/PaginaInventarioApplicationTests.java
@@ -0,0 +1,13 @@
+package Pagina_inventario.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaginaInventarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
